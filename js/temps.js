3ce24c1b6fe5b47de6da6ea2d3653a03fd89444b
Samuel Miranda <128534242+SaaS41@users.noreply.github.com>
2024-01-11 17:42:17 -0300

    Add files via upload

---
+++ js/temps.js
@@ -0,0 +1,10 @@
+const temp01 = document.getElementById('temp01');
+const temp02 = document.getElementById('temp02');
+
+temp01.addEventListener('click', ()=> {
+    window.location.href = 'html/pdp.html';
+});
+
+temp02.addEventListener('click', ()=> {
+    window.location.href = 'html/pdp_eclipse.html';
+});
---
+++ js/voltar.js
@@ -0,0 +1,5 @@
+const voltar = document.getElementById('btnVoltar');
+
+voltar.addEventListener('click', ()=> {
+    window.location.href = "../index.html";
+})
